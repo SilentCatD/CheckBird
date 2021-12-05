094c4bc6356e9a83fd2dbe064a3f727e2c51d77c
Silent Cat <ngocphuocnguyen312@gmail.com>
2021-12-05 14:34:45 +0700

    add home and authen base

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:check_bird/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,13 +16,5 @@ class MyApp extends StatelessWidget {
       ),
       home: const HomePage(),
     );
-  }
-}
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
   }
 }
---
+++ lib/screens/authentication/authenticate_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AuthenticateScreen extends StatelessWidget {
+  static const routeName = '/authenticate-screen';
+  const AuthenticateScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
