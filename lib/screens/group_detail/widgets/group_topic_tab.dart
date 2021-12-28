ea47643756cfa8e39c18c3473e003221df44e251
Silent Cat <ngocphuocnguyen312@gmail.com>
2021-12-28 21:29:35 +0700

    minor changes

--- lib/screens/group_detail/widgets/group_topic_tab.dart
+++ lib/screens/group_detail/widgets/group_topic_tab.dart
@@ -3,35 +3,46 @@ import 'package:flutter/material.dart';
 
 import 'create_post/create_post_screen.dart';
 
-class GroupTopicTab extends StatelessWidget {
+class GroupTopicTab extends StatefulWidget {
   const GroupTopicTab({Key? key, required this.groupId}) : super(key: key);
   final String groupId;
 
   @override
+  State<GroupTopicTab> createState() => _GroupTopicTabState();
+}
+
+class _GroupTopicTabState extends State<GroupTopicTab> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PostsLog(groupId: groupId,),
+      body: PostsLog(
+        groupId: widget.groupId,
+      ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () async {
           Navigator.of(context).push(
             PageRouteBuilder(
-                pageBuilder: (BuildContext context, Animation<double> animation,
-                        Animation<double> secondaryAnimation) => CreatePostScreen(groupId: groupId,),
-                transitionsBuilder:
-                    (context, animation, secondaryAnimation, child) {
-                  const begin = Offset(0.0, 1.0);
-                  const end = Offset.zero;
-                  const curve = Curves.ease;
+              pageBuilder: (BuildContext context, Animation<double> animation,
+                      Animation<double> secondaryAnimation) =>
+                  CreatePostScreen(
+                groupId: widget.groupId,
+              ),
+              transitionsBuilder:
+                  (context, animation, secondaryAnimation, child) {
+                const begin = Offset(0.0, 1.0);
+                const end = Offset.zero;
+                const curve = Curves.ease;
 
-                  var tween = Tween(begin: begin, end: end)
-                      .chain(CurveTween(curve: curve));
+                var tween = Tween(begin: begin, end: end)
+                    .chain(CurveTween(curve: curve));
 
-                  return SlideTransition(
-                    position: animation.drive(tween),
-                    child: child,
-                  );
-                }),
+                return SlideTransition(
+                  position: animation.drive(tween),
+                  child: child,
+                );
+              },
+            ),
           );
         },
       ),
